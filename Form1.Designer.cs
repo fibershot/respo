b47a661074e93fca823de90eb609bf49e985dd9b
Toni <48409314+fibershot@users.noreply.github.com>
2021-01-12 12:46:42 +0200

    The project

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace GUILearning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plusButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // plusButton
+            // 
+            this.plusButton.Location = new System.Drawing.Point(375, 204);
+            this.plusButton.Name = "plusButton";
+            this.plusButton.Size = new System.Drawing.Size(105, 30);
+            this.plusButton.TabIndex = 0;
+            this.plusButton.Text = "Do not press.";
+            this.plusButton.UseVisualStyleBackColor = true;
+            this.plusButton.Click += new System.EventHandler(this.plusButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 501);
+            this.Controls.Add(this.plusButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button plusButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUILearning
+{
+    public partial class Form1 : Form
+    {
+        private bool first = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void plusButton_Click(object sender, EventArgs e)
+        {
+            if (first == false) 
+            { 
+                MessageBox.Show("Stop pressing this button", "Leave");
+                plusButton.Text = ("Don't do it");
+                first = true;
+            }
+            else
+            {
+                MessageBox.Show("Shutting down.", "Shutdown");
+                Application.Exit();
+            }
+        }
+    }
+}
